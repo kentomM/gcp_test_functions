c3522e1f06f110cf7bbf8d156f0d83ba77fb97b1
Tomoki Mizuno <tomoki.mizuno@willgroup.co.jp>
2023-02-07 19:03:46 +0900

    ソースコードを郵便番号だけで分離しやすいように分割

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,75 +1,9 @@
-import * as cors from "cors"
-import * as express from "express"
-import * as admin from "firebase-admin"
 import * as functions from "firebase-functions"
 
-const app = express()
+import * as postalCodeAPI from "./postal-code/router/route"
 
-// cors対策
-app.use(cors({origin: true}))
-// jsonパーサの登録
-app.use(express.json())
-// firebase-adminの初期化
-admin.initializeApp()
-
-const db = admin.firestore()
-
-type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType[number]
-// ここの順番で返すオブジェクトのkeyの順番も変わる
-const addressKeys = [
-  "postal_code",
-  "address_1", "address_2", "address_3",
-  "kana_1", "kana_2", "kana_3",
-  "local_government_code",
-] as const
-type Address = {
-  [key in ArrayElement<typeof addressKeys>]: string
-}
-
-// POSTリクエストで登録処理（ドキュメントIDはユニーク）
-app.post("/addresses", async (req, res) => {
-  const setData: Address = {
-    postal_code: "0000001",
-    address_1: "東京都",
-    address_2: "中野区",
-    address_3: "本町",
-    kana_1: "ﾄｳｷｮｳﾄ",
-    kana_2: "ﾅｶﾉｸ",
-    kana_3: "ﾎﾝﾏﾁ",
-    local_government_code: "13114",
-  }
-  await db.collection("addresses").doc("0000001").set(setData)
-  res.send({message: "success"})
-})
-
-// データを取得
-app.get("/addresses", async (req, res) => {
-  const docs = await db.collection("addresses").get()
-  const data: Address[] = []
-  docs.forEach((doc) => {
-    const addresses = doc.data() as Address
-    // そのままだとkeyの順番がごちゃごちゃなのでソート
-    const sortedAddresses = addressKeys.reduce((obj, key) => {
-      obj[key] = addresses[key]
-      return obj
-    }, {} as Address)
-    data.push(sortedAddresses)
-  })
-  res.json({
-    total: data.length,
-    data,
-  })
-})
-
-// データを削除
-app.delete("/addresses", async (req, res) => {
-  const deletedPostalCode = "0000001"
-  await db.collection("addresses")
-      .doc(deletedPostalCode)
-      .delete()
-  res.send({message: "success"})
-})
-
-// apiが動くようにする
-const api = functions.region("asia-northeast1").https.onRequest(app)
+// 郵便番号apiが動くようにする
+const api = functions
+    .region("asia-northeast1")
+    .https.onRequest(postalCodeAPI.app)
 module.exports = {api}
